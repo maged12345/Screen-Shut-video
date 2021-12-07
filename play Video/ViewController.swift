179fd64f8f33434a8a498f892fc6afbb86d29932
maged <maged12345>
2021-12-07 08:36:47 -0800

    Initial Commit

---
+++ play Video/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  play Video
+//
+//  Created by maged mohamed on 12/7/21.
+//  Copyright © 2021 maged mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
